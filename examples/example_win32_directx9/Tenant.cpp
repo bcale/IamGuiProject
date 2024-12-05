ec218963cf15e71aedc65624e5e639fbd4207837
Caleb <caleb.barnes570@gmail.com>
2024-12-05 03:30:57 -0500

    Not sure

---
+++ examples/example_win32_directx9/Tenant.cpp
@@ -0,0 +1,28 @@
+#include "Tenant.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Tenant {
+private:
+    string tenant_first_name;
+    string tenant_last_name;
+public:
+    Tenant getTenant() {
+
+    }
+
+    void removeTenant() {
+
+    }
+
+    bool isEmpty() {
+
+    }
+
+    void addTenant() {
+
+    }
+};
---
+++ examples/example_win32_directx9/Tenant.h
@@ -0,0 +1,23 @@
+#pragma once
+class Tenant
+{
+private:
+
+public:
+    Tenant getTenant() {
+
+    }
+
+    void removeTenant() {
+
+    }
+
+    bool isEmpty() {
+
+    }
+
+    void addTenant() {
+
+    }
+};
+
