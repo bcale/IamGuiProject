c99cf5e8c03cb8bbe69bbd47b69d9b8d2d0c020b
Caleb <caleb.barnes570@gmail.com>
2024-12-05 03:00:08 -0500

    Not sure.

---
+++ examples/example_win32_directx9/Property.cpp
@@ -0,0 +1,22 @@
+#include "Property.h"
+#include "imgui.h"
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Property {
+private:
+    string address;
+
+public:
+    //default constructor
+    Property() {
+
+    };
+
+    void addProperty() {
+
+    };
+};
